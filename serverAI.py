939d2e8b9e6dd55850a54531f4a30b4bff757532
Raushan Kumar <146509821+raushgit@users.noreply.github.com>
2025-08-18 23:18:19 +0530

    Update serverAI.py

--- serverAI.py
+++ serverAI.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
-import time
+import time 
  
 
 class ServerAI:
@@ -60,6 +60,7 @@ class ServerAI:
 # Create an instance of ServerAI and start the server
 server = ServerAI()
 server.start_server()
+
 
 
 
